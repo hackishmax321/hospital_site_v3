d250e6a2d237443ef52ba3d55dca077b0b8e109b
hackishmax <hackishmax321@gmail.com>
2020-10-19 23:31:39 +0530

    Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { InfoComponent } from './components/pages/info/info.component';
+import { MedsComponent } from './components/pages/meds/meds.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'information', component:InfoComponent},
+  {path:'medicines', component:MedsComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'about', component:AboutComponent},
+  {path:'**', component:ErrorComponent}
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MedsComponent } from './components/pages/meds/meds.component';
+import { InfoComponent } from './components/pages/info/info.component';
+import { ServiceComponent } from './components/pages/service/service.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ErrorComponent } from './components/pages/error/error.component';
+import { NavigationComponent } from './components/sections/navigation/navigation.component';
+import { SlideComponent } from './components/sections/slide/slide.component';
+import { FooterComponent } from './components/sections/footer/footer.component';
+import { RouterModule} from '@angular/router';
+import { ContainerDisplayerComponent } from './components/sections/container-displayer/container-displayer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MedsComponent,
+    InfoComponent,
+    ServiceComponent,
+    AboutComponent,
+    ContactComponent,
+    ErrorComponent,
+    NavigationComponent,
+    SlideComponent,
+    FooterComponent,
+    ContainerDisplayerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
